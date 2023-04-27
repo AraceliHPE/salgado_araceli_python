c476ddf2ba99d7650274e1cb0fdb5c2067f271be
AraceliHPE <131091860+AraceliHPE@users.noreply.github.com>
2023-04-26 18:18:50 -0600

    Add files via upload

---
+++ ejercicio_001.py
@@ -0,0 +1,10 @@
+nombre = "Araceli"
+apellido= "Salgado"
+apellido2= "Camacho"
+Año= 1973
+Mes_dia= 9-17
+nombre_lower= nombre.lower()
+nombre_upper= nombre.upper()
+AÑO_ACTUAL=2022
+print(nombre_lower)
+print(nombre_upper)
